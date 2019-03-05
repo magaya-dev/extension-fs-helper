7c9e142e739abcf62fa75ca871d16dc4fb9801cb
Ulises Gonzalez <ugonzalez@magaya.com>
2019-03-05 16:57:36 -0500

    Initialized package

---
+++ index.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const path = require('path');
+
+function isExtensionConfig(result, extensionId) {
+    if (!fs.existsSync(result))
+        return false;
+
+    const extensionConfig = require(result);
+    if (!extensionConfig || !extensionConfig.id)
+        return false;
+
+    return (extensionConfig.id.name === extensionId.name && extensionConfig.id.company === extensionId.company);
+}
+
+function GetExtensionFolder(currentFolder, extensionId) {
+    let result =  currentFolder;
+    let extConfigFile = currentFolder + '\\extension.config.json';
+    while (!isExtensionConfig(extConfigFile, extensionId)) {
+        let upFolder = path.join(currentFolder, '..');
+        if (upFolder != currentFolder) {
+            result = upFolder;
+        }
+        else
+            break;
+    }
+    return result;
+}
+
+function CreatePath(currentFolder, targetPath) { 
+    const sep = /[\/|\\]/g;
+    return targetPath.split(sep).reduce((parentDir, childDir) => {
+            curDir = path.join(parentDir, childDir);
+            if (!fs.existsSync(curDir)) {
+                fs.mkdirSync(curDir);
+            }
+            return curDir;
+        }
+        , currentFolder
+    );
+}
+
+function GetExtensionDataFolder(extensionId, networkID){
+    //if installed in Magaya, get or create a Folder in ../Magaya Corp/"extensions-data"/extensionDataFolder/networkId
+    //otherwise ../extensionFolder/"extensions-data"/extensionDataFolder/networkId
+    let nodeFolder = process.cwd();
+
+    if (!extensionId || !extensionId.name || !extensionId.company) 
+        throw new Error('Please specify the extension that will be installed in Magaya.');
+    if (!networkID)   
+        throw new Error('Please specify Communication Suite NetworkId that this extension belongs to.');
+
+    let currentFolder = GetExtensionFolder(path.join(nodeFolder, '..'), extensionId);
+    if (!fs.existsSync(currentFolder))
+        currentFolder = nodeFolder;
+    let upFolder = path.join(currentFolder, '..');
+    upFolderName = path.basename(upFolder);
+    if (upFolderName === 'extensions') {
+        currentFolder = path.join(upFolder, '..\\..');
+    }
+    //Add always NetworkID after Extension Data folder in case more than one CS is running on the same PC
+    let myExtensionDataPath = 'extensions-data/' + extensionId.company + '-' + extensionId.name + '/' + networkID;
+    let result = path.join(currentFolder, myExtensionDataPath);
+    if (!fs.existsSync(result)) {
+        result = CreatePath(currentFolder, myExtensionDataPath);
+    }
+    return result;
+}
+
+module.exports = {
+    GetExtensionDataFolder : GetExtensionDataFolder
+};
+
